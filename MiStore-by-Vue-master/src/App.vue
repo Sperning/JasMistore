ba0d5b31e06097c9d6413436caa88b7bb44291ab
Sperning <971622924@qq.com>
2019-04-16 21:05:10 +0800

    Mistore

---
+++ MiStore-by-Vue-master/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <topBar></topBar>
+    <topHeader></topHeader>
+    <mainPage></mainPage>
+
+  </div>
+</template>
+
+<script>
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+import TopBar from './components/TopBar'
+import TopHeader from './components/TopHeader'
+import MainPage from './components/MainPage'
+
+export default {
+  components: {
+    'topBar': TopBar,
+    'topHeader': TopHeader,
+    'mainPage': MainPage,
+    // 'miFooter': MiFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Helvetica Neue", Helvetica, Arial, "Microsoft Yahei", "Hiragino Sans GB", "Heiti SC";
+}
+</style>
